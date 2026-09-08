3b7ae4b374c9dce0af13dbfbee33073e0ead1504
Prashant Koli <koliprashant11112@gmail.com>
2026-09-08 18:18:39 +0530

    Health controller Added

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.Controllers
+{
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Health()
+        {
+            return Ok("Healthy");
+        }
+    }
+}
